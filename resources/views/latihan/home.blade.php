d67ebcb66a8c8a5b184e4abae01452266b67ed1f
bilalala777 <541231149@student.smktelkom-pwt.sch.id>
2024-10-22 10:42:17 +0700

    first commit

---
+++ resources/views/latihan/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>i hope everytime you heard gracie abrams, you think about me</h1>
+</body>
+</html>
